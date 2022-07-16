d287740d718bb16d484f6c390d5e8c3a11c5b026
Daniel Lopes <ddanielssoares@gmail.com>
2022-07-16 01:34:41 -0300

    Life System with HelthBar implemented

--- Assets/Scripts/Controllers/Enemies/EnemyController.cs
+++ Assets/Scripts/Controllers/Enemies/EnemyController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -8,44 +9,126 @@ public class EnemyController : MonoBehaviour
     [Header("Animation")]
     [SerializeField] private Animator animator;
 
-    [Header("Distance")]
-    [SerializeField] private float distanceStopRun;
+    [Header("Stats")]
+    [SerializeField] private float damage;
+    [SerializeField] private float playerVisionRay;
 
     private Rigidbody2D rb;
     private Transform playerPosition;
+    private Transform objectivePosition;
     private SpriteRenderer sr;
+    private bool isAttackingPlayer = false;
+    private bool isAttackingObjective = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        objectivePosition = GameObject.FindGameObjectWithTag("Objective").transform;
         sr = GetComponent<SpriteRenderer>();
     }
     void Update()
     {
-        if (Vector2.Distance(transform.position, playerPosition.position) <= distanceStopRun)
+        if (!isAttackingObjective && !isAttackingPlayer)
         {
-            animator.SetBool("isRunning", false);
-            rb.velocity = Vector2.zero;
+            var target = GetTarget();
+            FollowTarget(target);
+            SetDirectionSprite(target);
         }
         else
         {
-            FollowPlayer();
+            SetAnimationRunning(false);
+            rb.velocity = Vector2.zero;
         }
     }
-
-    private void FollowPlayer()
+    private Transform GetTarget()
     {
-        animator.SetBool("isRunning", true);
+        var entities = GetEntitiesOnVision();
+        Transform target = objectivePosition;
 
-        if (rb.velocity.x > 0)
+        if (entities.Count > 0)
+        {
+            float minDistance = float.MaxValue;
+            foreach (var entity in entities)
+            {
+                var distance = CalculateDistance(entity);
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    target = entity;
+                }
+            }
+        }
+        return target;
+    }
+    private List<Transform> GetEntitiesOnVision()
+    {
+        List<Transform> entities = new List<Transform>();
+        
+        if (IsPlayerInVisionArea())
+        {
+            entities.Add(playerPosition);
+        }
+        
+        return entities;
+    }
+    private float CalculateDistance(Transform entity)
+    {
+        return Vector2.Distance(transform.position, entity.position);
+    }
+    private bool IsPlayerInVisionArea()
+    {
+        return Vector2.Distance(transform.position, playerPosition.position) <= playerVisionRay;
+    }
+    private bool IsOnRightSide(Transform entity)
+    {
+        return entity.position.x > transform.position.x;
+    }
+    private void SetDirectionSprite(Transform entity)
+    {
+        if (IsOnRightSide(entity))
         {
             sr.flipX = false;
         }
-        else if (rb.velocity.y < 0)
+        else
         {
             sr.flipX = true;
         }
+    }
+    private void SetAnimationRunning(bool play)
+    {
+        animator.SetBool("isRunning", play);
+    }
+    private void FollowTarget(Transform target)
+    {
+        SetAnimationRunning(true);
 
-        transform.position = Vector2.MoveTowards(transform.position, playerPosition.position, moveSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !isAttackingObjective)
+        {
+            isAttackingPlayer = true;
+            if (collision.gameObject.GetComponent<LifeSystem>().TakeDamage(damage))
+            {
+                Debug.Log("Player is dead!");
+            }
+        }
+        else if (collision.gameObject.CompareTag("Objective") && !isAttackingPlayer)
+        {
+            isAttackingObjective = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isAttackingPlayer = false;
+        }
+        else if (collision.gameObject.CompareTag("Objective"))
+        {
+            isAttackingObjective = false;
+        }
     }
 }
---
+++ Assets/Scripts/GameMechanics/LifeSystem/LifeSystem.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeSystem : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField] public float maxLife;
+    [SerializeField] public float currentLife;
+    [SerializeField] public Slider HealtBar;
+
+    private void Start()
+    {
+        currentLife = maxLife;
+        HealtBar.maxValue = maxLife;
+        HealtBar.value = currentLife;
+    }
+    public bool TakeDamage(float damage)
+    {
+        Debug.Log("Take damage!");
+        currentLife -= damage;
+        UpdateHealthBar();
+        if (currentLife <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public void UpdateHealthBar()
+    {
+        HealtBar.value = currentLife;
+    }
+}
