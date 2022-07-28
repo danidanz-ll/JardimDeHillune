14ca8b34c02c01269a7a16655b5d154e47c1b5ae
Daniel Nardi <69864998+danidanz-ll@users.noreply.github.com>
2022-07-28 14:19:13 -0300

    Obelisco DIa e Noite

---
+++ Assets/Game/Scripts/Animated Obelisc/AnimatedTile.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+[CreateAssetMenu(menuName = "Tile.../Animated Tile", fileName = "New Animated Tile")]
+public class AnimatedTile : TileBase
+{
+    public Sprite[] sprites;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        if(sprites != null && sprites.Length > 0)
+        {
+            tileData.sprite = sprites[0];
+        }
+    }
+
+    public override bool GetTileAnimationData(Vector3Int position, ITilemap tilemap, ref TileAnimationData tileAnimationData)
+    {
+        if(sprites.Length > 0)
+        {
+            tileAnimationData.animatedSprites = sprites;
+            tileAnimationData.animationSpeed = 1;
+            tileAnimationData.animationStartTime = 0;
+            
+            return true;
+        }
+
+        return false;
+
+    }
+}
+ 
