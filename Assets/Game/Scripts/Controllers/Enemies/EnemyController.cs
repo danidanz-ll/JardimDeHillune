f812cad1354ef6239334ce5de32d324be0fb4bbf
Daniel Lopes <ddanielssoares@gmail.com>
2022-07-29 00:55:19 -0300

    Migrado código de morte

--- Assets/Game/Scripts/Controllers/Enemies/EnemyController.cs
+++ Assets/Game/Scripts/Controllers/Enemies/EnemyController.cs
@@ -3,25 +3,27 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour, ICharacterController
 {
     [Header("Config")]
-    [SerializeField]
-    GameObject weaponObject;
+    [SerializeField] GameObject weaponObject;
 
     public bool IsDead = false;
     private EnemyMovement enemyMovement;
     private LifeSystem lifeSystem;
     private IDamageable damageable;
+    public IMortal deathOnDamage { get; private set; };
     private IWeapon weapon;
     void Start()
     {
         enemyMovement = GetComponent<EnemyMovement>();
         lifeSystem = GetComponent<LifeSystem>();
         damageable = GetComponent<IDamageable>();
+        damageable = GetComponent<IMortal>();
         if (weaponObject != null)
         {
             weapon = weaponObject.GetComponent<IWeapon>();
         }
 
         damageable.DamageEvent += OnDamage;
+        deathOnDamage.DeathEvent += OnDeath;
     }
 
     private void Update()
@@ -38,6 +40,10 @@ public class EnemyController : MonoBehaviour, ICharacterController
         {
             damageable.DamageEvent -= OnDamage;
         }
+        if (deathOnDamage != null)
+        {
+            deathOnDamage.DeathEvent -= OnDeath;
+        }
     }
     public void SetMovement(Vector2 direction)
     {
@@ -52,10 +58,13 @@ public class EnemyController : MonoBehaviour, ICharacterController
         weapon.Attack();
         enemyMovement.FreezeMovement(weapon.GetWaitToFreezeTime(), weapon.GetAttackingTime());
     }
-    private void OnDamage()
+    private void OnDamage(float damage)
     {
         enemyMovement.StopMovement();
-        lifeSystem.TakeDamage(10);
+        if(!lifeSystem.TakeDamage(damage))
+        {
+            OnDeath();
+        }
     }
     private void OnDeath()
     {
--- Assets/Game/Scripts/GameMechanics/DamageSystem/TakeOnDamage.cs
+++ Assets/Game/Scripts/GameMechanics/DamageSystem/TakeOnDamage.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 public class TakeOnDamage : MonoBehaviour, IDamageable
 {
     public event Action DamageEvent;
-
-    private bool IsHurting = false;
-
+    private bool IsHurting { get; private set; } = false;
     public bool IsTakingDamage()
     {
         return IsHurting;
     }
-
     public void TakeDamage(float damage)
     {
-        DamageEvent.Invoke();
+        DamageEvent.Invoke(damage);
     }
 }
