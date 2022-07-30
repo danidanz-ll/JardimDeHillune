b89064199f3a4d171ed92edeb7303c581a47db49
Daniel Lopes <ddanielssoares@gmail.com>
2022-07-30 20:13:59 -0300

    GameOver mechanic

---
+++ Assets/Game/Scripts/GameMechanics/MatchManager/GameOver.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    private GameObject Player;
+    private GameObject Objective;
+    private LifeSystem PlayerLife;
+    private LifeSystem ObjectiveLife;
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Objective = GameObject.FindGameObjectWithTag("Objective");
+        PlayerLife = Player.GetComponent<LifeSystem>();
+        ObjectiveLife = Objective.GetComponent<LifeSystem>();
+    }
+    private void Start()
+    {
+        if (PlayerLife.currentLife <= 0 || ObjectiveLife.currentLife <= 0)
+        {
+            Debug.Log("Game Over!");
+        }
+    }
+}
