4bb7c9411465cb3ffc0c4a20b3acd51d581340ab
Daniel Lopes <ddanielssoares@gmail.com>
2022-08-19 23:34:26 -0300

    Atualização para nova api

--- Assets/Game/Scripts/GameMechanics/DamageSystem/TriggerDamage.cs
+++ Assets/Game/Scripts/GameMechanics/DamageSystem/TriggerDamage.cs
@@ -8,10 +8,17 @@ public class TriggerDamage : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         IDamageable damageable = collision.GetComponent<IDamageable>();
-        GameObject collisionGameObject = collision.collider.gameObject;
+        GameObject collisionGameObject = collision.GetComponent<Collider>().gameObject;
         if (damageable != null)
         {
-            if (collisionGameObject.tag != "Enemy")
+            if (isEnemy)
+            {
+                if (collisionGameObject.tag != "Enemy")
+                {
+                    damageable.TakeDamage(damage);
+                }
+            }
+            else
             {
                 damageable.TakeDamage(damage);
             }
