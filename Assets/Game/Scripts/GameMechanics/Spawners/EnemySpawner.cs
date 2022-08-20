0811471798b475ef0bcc5823291c649e5229807c
Daniel Lopes <ddanielssoares@gmail.com>
2022-08-19 21:57:05 -0300

    Limitação do spawn dos inimigos

--- Assets/Game/Scripts/GameMechanics/Spawners/EnemySpawner.cs
+++ Assets/Game/Scripts/GameMechanics/Spawners/EnemySpawner.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class EnemySpawner : MobSpawner
 {
+    [SerializeField] private float angleRangeToSpawn = 30.0f;
     public override void Start()
     {
         base.Start();
@@ -16,6 +17,33 @@ public class EnemySpawner : MobSpawner
     }
     public Vector3 GetRandomPositionSpawn()
     {
-        return new Vector3(LenghtMap * Random.Range(-LenghtMap, LenghtMap), LenghtMap * Random.Range(-LenghtMap, LenghtMap), 0);
+        float x, y, angle;
+        do
+        {
+            x = Random.Range(0, LenghtMap);
+            y = Random.Range(0, LenghtMap);
+            angle = Mathf.Atan2(x, y) * Mathf.Rad2Deg;
+        } while (angle >= angleRangeToSpawn);
+
+        if (Random.Range(-1, 1) < 0)
+        {
+            x *= -1;
+        }
+
+        if (Random.Range(-1, 1) < 0)
+        {
+            y *= -1;
+        }
+
+        return new Vector3(x, y, 0);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, LenghtMap);
+
+        Gizmos.DrawLine(transform.position, transform.position + (Quaternion.Euler(0, 0, angleRangeToSpawn / 2));
+        Gizmos.DrawLine(transform.position, transform.position + (Quaternion.Euler(0, 0, -angleRangeToSpawn / 2));
+        Gizmos.DrawLine(transform.position, transform.position - (Quaternion.Euler(0, 0, angleRangeToSpawn / 2));
+        Gizmos.DrawLine(transform.position, transform.position - (Quaternion.Euler(0, 0, -angleRangeToSpawn / 2));
     }
 }
