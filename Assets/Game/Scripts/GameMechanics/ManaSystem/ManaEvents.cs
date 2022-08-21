b59d02ddf742383052894ec9f272b02e5e5c42ea
Daniel Lopes <ddanielssoares@gmail.com>
2022-08-20 22:51:03 -0300

    Correção no nome do evento

--- Assets/Game/Scripts/GameMechanics/ManaSystem/ManaEvents.cs
+++ Assets/Game/Scripts/GameMechanics/ManaSystem/ManaEvents.cs
@@ -55,6 +55,6 @@ public class ManaEvents : MonoBehaviour
     }
     public void UseMana(float points)
     {
-        DamageValueEvent.Invoke(this, points);
+        UseManaEvent.Invoke(this, points);
     }
 }
