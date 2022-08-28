faded9d597167e609478607b7f821242f05815a2
Daniel Lopes <ddanielssoares@gmail.com>
2022-08-28 17:17:27 -0300

    Controlador do boss

---
+++ Assets/Game/Scripts/Controllers/Boss/BossController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using UnityEngine;
+
+public class BossController : MonoBehaviour, ICharacterController
+{
+    [Header("Weapon")]
+    [SerializeField] GameObject weaponObject;
+    [Header("Death settings")]
+    [Min(0)]
+    [SerializeField] private float TimeToDisappearAfterDeath = 0;
+
+    public bool IsDead = false;
+    private EnemyMovement enemyMovement;
+    private LifeSystem lifeSystem;
+    private IDamageable damageable;
+    public IMortal mortal { get; private set; }
+    private IWeapon weapon;
+    private TowerSkill towerSkill;
+    void Start()
+    {
+        enemyMovement = GetComponent<EnemyMovement>();
+        lifeSystem = GetComponent<LifeSystem>();
+        damageable = GetComponent<IDamageable>();
+        mortal = GetComponent<IMortal>();
+        towerSkill = GetComponent<TowerSkill>();
+        if (weaponObject != null)
+        {
+            weapon = weaponObject.GetComponent<IWeapon>();
+        }
+
+        mortal.DeathEvent += OnDeath;
+        mortal.RessurectEvent += Resurrect;
+    }
+    private void OnDestroy()
+    {
+        if (mortal != null)
+        {
+            mortal.DeathEvent -= OnDeath;
+            mortal.RessurectEvent -= Resurrect;
+        }
+    }
+    public void SetMovement(Vector2 direction)
+    {
+        enemyMovement.SetMovement(direction);
+    }
+    public float GetMovementSpeed()
+    {
+        return enemyMovement.moveSpeed;
+    }
+    public void Attack()
+    {
+        weapon.Attack();
+        enemyMovement.FreezeMovement(0, weapon.GetAttackingTime());
+    }
+    private void OnDeath()
+    {
+        enemyMovement.SetBodyType(RigidbodyType2D.Static);
+    }
+    private void Resurrect()
+    {
+        enemyMovement.SetBodyType(RigidbodyType2D.Dynamic);
+        lifeSystem.SetFullLife();
+    }
+    public bool CharacterIsDead()
+    {
+        return mortal.IsDead;
+    }
+    private IEnumerator DisappearAfterDeath()
+    {
+        yield return new WaitForSeconds(TimeToDisappearAfterDeath);
+    }
+    public void InvokeEnemy()
+    {
+        Vector2 aux = enemyMovement.GetFacingDirection();
+        Vector3 aux2 = new Vector3(aux.x, aux.y, 0);
+        towerSkill.Invoke(transform.position + aux2);
+    }
+}
